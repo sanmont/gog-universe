66ac9dfcc2034a940010d3a087f11068ea2b263f
santiago <santiago@Debby.myguest.virtualbox.org>
2024-01-10 13:00:42 -0300

    ISSUE #1 - Created enviroment to build and run electron app

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-
+import {app, BrowserWindow} from 'electron';
+// @ts-ignore
+import htmlFile from '../app/app.html';
 
 const createWindow = () => {
 	const win = new BrowserWindow({
@@ -7,7 +8,7 @@ const createWindow = () => {
 	  height: 600
 	})
   
-	win.loadFile('out/app.html')
+	win.loadFile(htmlFile);
 }
 
 app.whenReady().then(() => {
